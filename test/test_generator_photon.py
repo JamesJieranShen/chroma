615dc1cda3f1c70df7059cee73413156f7b3f236
James Shen <jamesjieranshen@gmail.com>
2023-07-05 18:27:44 +0800

    Add multicore generator test case

--- test/test_generator_photon.py
+++ test/test_generator_photon.py
@@ -19,3 +19,13 @@ class TestG4ParallelGenerator(unittest.TestCase):
         vertex = itertools.islice(constant_particle_gun('e-', (1000,0,0), (1,0,0), 100), 10)
         for event in gen.generate_events(vertex):
             self.assertGreater(len(event.photons_beg.pos), 0)
+
+    def test_multicore(self):
+        gen = generator.photon.G4ParallelGenerator(4, water)
+        num_events = 100
+        vertex = itertools.islice(constant_particle_gun('e-', (0, 0, 0), (1, 0, 0), 100), num_events)
+        num_photons = 0
+        for event in gen.generate_events(vertex):
+            num_photons += len(event.photons_beg.pos)
+            self.assertGreater(len(event.photons_beg.pos), 0)
+        print(f"Average number of photons per event: {num_photons/num_events}")
