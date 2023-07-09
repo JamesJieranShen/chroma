f555ee17b64e9f9015a14b1101d86befc4459569
James Shen <jamesjieranshen@gmail.com>
2023-07-09 17:27:05 +0800

    Conda-based build environments

---
+++ installation/buildchain-update/conda-geant4/31-setup_g4py.sh
@@ -0,0 +1,1 @@
+export PYTHONPATH=/opt/geant4/g4python/site-packages:$PYTHONPATH
---
+++ installation/buildchain-update/conda/11-conda_activate.sh
@@ -0,0 +1,4 @@
+conda activate chroma
+export CPATH="/opt/miniconda3/envs/chroma/include:$CPATH"
+export LIBRARY_PATH="$LIBRARY_PATH:/opt/miniconda3/envs/chroma/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/miniconda3/envs/chroma/lib"
